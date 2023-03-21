07989616e5e2ac51ba30236cba03f7e0c01a6108
应卓 <yingzhor@gmail.com>
2023-03-21 11:41:06 +0800

    2023-03-21 11:41:06

---
+++ cloud-config-server-3344/src/main/java/cloud/config/server/ConfigServerMain3344.java
@@ -0,0 +1,13 @@
+package cloud.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerMain3344 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerMain3344.class, args);
+    }
+
+}
